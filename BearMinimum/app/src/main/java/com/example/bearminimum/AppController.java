c2647db4e5d80f4cfe2ca2d01f03d40613599bdf
htrinh1 <htrinh1@ualberta.ca>
2020-11-06 14:39:48 -0700

    Documented AppController

--- BearMinimum/app/src/main/java/com/example/bearminimum/AppController.java
+++ BearMinimum/app/src/main/java/com/example/bearminimum/AppController.java
@@ -8,6 +8,15 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
+/**
+ * AppController
+ *
+ * Raises an small error message if can't connect
+ * to Google book api
+ *
+ * Nov. 6, 2020
+ */
+
 public class AppController {
 
     private static com.example.bearminimum.AppController mInstance;
@@ -21,12 +30,26 @@ public class AppController {
         mRequestQueue = getRequestQueue();
     }
 
+    /**\
+     * creates an instance of the AppController if it doesn't already exist
+     *
+     * @param context
+     * @return mInstance    the AppController instance
+     */
+
     public static synchronized com.example.bearminimum.AppController getInstance(Context context) {
         if (mInstance == null) {
             mInstance = new com.example.bearminimum.AppController(context);
         }
         return mInstance;
     }
+
+
+    /**
+     * creates a request queue if it doesn't already exist
+     *
+     * @return mRequestQueue    the request queue instance
+     */
 
     public RequestQueue getRequestQueue() {
         if (mRequestQueue == null) {
@@ -35,9 +58,26 @@ public class AppController {
         return mRequestQueue;
     }
 
+
+    /**
+     * adds a request to the request queue
+     *
+     * @param request
+     * @param <T>
+     */
+
     public <T> void addToRequestQueue(@NonNull final Request<T> request) {
         getRequestQueue().add(request);
     }
+
+
+    /**
+     * adds a request to the requests queue along with a tag
+     *
+     * @param request
+     * @param tag
+     * @param <T>
+     */
 
     public <T> void addToRequestQueueWithTag(@NonNull final Request<T> request, String tag) {
         request.setTag(tag);
