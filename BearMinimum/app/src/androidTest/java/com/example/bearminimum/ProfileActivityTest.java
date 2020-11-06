c67ff15e5b2de379545cf0e6c2e3ecf1ec3ec489
Rylan Chin <rchin@ualberta.ca>
2020-11-06 15:38:39 -0700

    work on profile test

--- BearMinimum/app/src/androidTest/java/com/example/bearminimum/ProfileActivityTest.java
+++ BearMinimum/app/src/androidTest/java/com/example/bearminimum/ProfileActivityTest.java
@@ -5,11 +5,16 @@ import android.app.Activity;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.robotium.solo.Solo;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class ProfileActivityTest {
     private Solo solo;
@@ -24,6 +29,9 @@ public class ProfileActivityTest {
     @Before
     public void setUp() throws Exception {
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword("test@bearmin.com", "test123");
+        }
     }
 
     /**
@@ -35,5 +43,24 @@ public class ProfileActivityTest {
         Activity activity = rule.getActivity();
     }
 
-    
+    /**
+     *
+     */
+    @Test
+    public void checkCurrentUserProfile() {
+        // Asserts that the current activity is the MainActivity. Otherwise, show “Wrong Activity”
+        solo.assertCurrentActivity("Wrong Activity", ProfileActivity.class);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        assertTrue(solo.waitForText(user.getEmail(),1,2000));
+        assertTrue(solo.waitForText(user.getDisplayName(),1,2000));
+    }
+
+    @After
+    public void tearDown() {
+        if (FirebaseAuth.getInstance().getCurrentUser() != null)
+            FirebaseAuth.getInstance().signOut();
+        solo.finishOpenedActivities();
+    }
 }
