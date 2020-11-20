f9714991f0efc429185436016f05e9117279e719
badacetate <bech@ualberta.ca>
2020-11-19 17:04:58 -0700

    Update MapActivity.java

--- BearMinimum/app/src/main/java/com/example/bearminimum/MapActivity.java
+++ BearMinimum/app/src/main/java/com/example/bearminimum/MapActivity.java
@@ -14,25 +14,27 @@ public class MapActivity extends MainActivity implements OnMapReadyCallback{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_maps);
+
         // Get the SupportMapFragment and request notification
         // when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        // Add a marker in Sydney, Australia,
+        //set map type
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        // Add a misc marker
         // and move the map's camera to the same location.
-        LatLng sydney = new LatLng(-33.852, 151.211);
+        LatLng misc = new LatLng(0, 0);
         googleMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney"));
-        // [START_EXCLUDE silent]
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        // [END_EXCLUDE]
+                .position(misc)
+                .title("generic"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(misc));
     }
 
 }
