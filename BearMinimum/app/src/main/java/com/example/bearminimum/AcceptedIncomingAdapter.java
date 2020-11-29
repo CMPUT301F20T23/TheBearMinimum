754a2dc66bc2c161aa7964793ff019e7cf2491e5
Rikyyyyyyyyyy <43457622+Rikyyyyyyyyyy@users.noreply.github.com>
2020-11-28 21:38:42 -0700

    fff

---
+++ BearMinimum/app/src/main/java/com/example/bearminimum/AcceptedIncomingAdapter.java
@@ -0,0 +1,183 @@
+package com.example.bearminimum;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+
+/**
+ * AcceptedIncomingAdapter
+ *
+ * basic adapter extending RecyclerView.Adapter
+ * custom ViewHolder to access our views
+ * Used to display books in the MainActivity RecyclerView
+ *
+ */
+
+public class AcceptedIncomingAdapter extends RecyclerView.Adapter<AcceptedIncomingAdapter.ViewHolder> {
+    /**
+     * Interface
+     * OnBookClickListener
+     *
+     */
+    public Context context;
+
+    public interface OnBookClickListener {
+        void onBookClick(int position, String owner);
+    }
+
+
+    //item click listener
+    private OnBookClickListener mOnBookClickListener;
+
+    //member variable for book objects
+    private List<Book> mBooks;
+
+
+    //pass in the book array to the constructor
+    public AcceptedIncomingAdapter(List<Book> books, OnBookClickListener onBookClickListener) {
+        mBooks = books;
+        this.mOnBookClickListener = onBookClickListener;
+    }
+
+    //direct reference to each view within a data item
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        //declare listener
+        OnBookClickListener onBookClickListener;
+
+        //member variable for views rendered as rows
+        public TextView bookTextView;
+        public TextView userNameTextView;
+        public TextView statusTextView;
+        public Button scanButton;
+
+        //constructor that accepts new row
+        //view lookups to find each subview
+        public ViewHolder(View itemView, OnBookClickListener onBookClickListener) {
+            super(itemView);
+
+            bookTextView = (TextView) itemView.findViewById(R.id.air_book_name);
+            userNameTextView = (TextView) itemView.findViewById(R.id.air_requester);
+            statusTextView = (TextView) itemView.findViewById(R.id.air_location_status);
+            scanButton = (Button) itemView.findViewById(R.id.air_scan_button);
+            this.onBookClickListener = onBookClickListener;
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            onBookClickListener.onBookClick(getAdapterPosition(), mBooks.get(getAdapterPosition()).getOwner());
+        }
+    }
+
+
+
+    //adapter requires three primary methods
+    //onCreateViewHolder to inflate and create the holder
+    //onBindViewHolder to set the view attributes based on data
+    //getItemCount to determine number of items
+
+    @Override
+    public AcceptedIncomingAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        //inflate the custom layout
+        View contactView = inflater.inflate(R.layout.accepted_incoming_list, parent, false);
+
+        //return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView, mOnBookClickListener);
+        return viewHolder;
+    }
+
+    //populate data into the item through holder
+    @Override
+    public void onBindViewHolder(AcceptedIncomingAdapter.ViewHolder holder, int position) {
+
+        //get data based on position
+        Book book = mBooks.get(position);
+        Log.d("MyDebug", book.getLatitude());
+
+        //set item views based on views and data
+        TextView textView1 = holder.bookTextView;
+        textView1.setText(book.getTitle());
+        TextView textView2 = holder.userNameTextView;
+        TextView textView3 = holder.statusTextView;
+
+        FirebaseFirestore.getInstance().collection("books").document(book.getBid()).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (value.exists()) {
+                    String locationStatus = (String) value.getData().get("latitude");
+                    if (locationStatus.length() > 0) {
+                        textView3.setText("location status: selected");
+                    } else {
+                        textView3.setText("location status: select a location");
+                    }
+                }
+            }
+        });
+
+        FirebaseFirestore.getInstance().collection("books").document(book.getBid()).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            String requester = ((List<String>) task.getResult().getData().get("requests")).get(0);
+                            FirebaseFirestore.getInstance().collection("users").document(requester).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                    textView2.setText("requested by: " + task.getResult().getData().get("username"));
+                                }
+                            });
+                        } else
+                            textView2.setText("requested by: failed to fetch");
+                    }
+                });
+
+        Button scanBtn = holder.scanButton;
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //launch scan activity
+                Log.i("Riky","here I am");
+                Intent intent = new Intent(view.getContext(),BarCodeHelper_outgoing.class);
+                ((Activity) view.getContext()).startActivityForResult(intent, 1);
+
+            }
+        });
+    }
+    //return total count of items in the list
+    @Override
+    public int getItemCount() {
+        return mBooks.size();
+    }
+
+
+}
---
+++ BearMinimum/app/src/main/java/com/example/bearminimum/AcceptedIncomingReqs.java
@@ -0,0 +1,135 @@
+package com.example.bearminimum;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class AcceptedIncomingReqs extends AppCompatActivity implements AcceptedIncomingAdapter.OnBookClickListener{
+
+    private RecyclerView recyclerView;
+    private AcceptedIncomingAdapter adapter;
+    private ArrayList<Book> bookData;
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private CollectionReference booksRef;
+    private FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accepted_incoming_reqs);
+
+        bookData = new ArrayList<>();
+        initAdapter();
+
+        booksRef = FirebaseFirestore.getInstance().collection("books");
+        Query query = booksRef.whereEqualTo("owner", user.getUid()).whereEqualTo("status", "accepted");
+        query.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                bookData.clear();
+                if (!value.isEmpty()) {
+                    for (QueryDocumentSnapshot doc : value) {
+                        Map data = doc.getData();
+                        Log.d("MyDebug", "requests exist");
+                        String title = (String) data.get("title");
+                        String author = (String) data.get("author");
+                        String bid = (String) data.get("bookid");
+                        String desc = (String) data.get("description");
+                        String isbn = (String) data.get("isbn");
+                        String status = (String) data.get("status");
+                        String borrower = (String) data.get("borrower");
+                        String lat = (String) data.get("latitude");
+                        String owner_scan=(String)doc.getData().get("owner_scan") ;
+                        String borrower_scan=(String)doc.getData().get("borrower_scan") ;
+                        Log.d("MyDebug", "lat is: " + lat);
+                        String longitude = (String) data.get("longitude");
+                        bookData.add(new Book(title, author, user.getUid(), borrower, desc, isbn, status, bid, lat, longitude, owner_scan, borrower_scan));
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent D) {
+        super.onActivityResult(requestCode, resultCode, D);
+
+        if (resultCode == 4){
+            db = FirebaseFirestore.getInstance();
+            Log.i("Riky","Tester6666");
+            String isbn_num = D.getStringExtra("isbn_number");
+            db.collection("books").whereEqualTo("owner", user.getUid())
+                    .whereEqualTo("isbn", isbn_num)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot doc : task.getResult()){
+                                    doc.getReference().update("status", "available");
+                                    Log.d("AcceptedIncomingReqsLA", "status changed to available");
+                                }
+
+                            }
+                        }
+                    });
+            
+        }
+    }
+
+
+    private void initAdapter() {
+        recyclerView = findViewById(R.id.accepted_incoming_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
+        dividerItemDecoration.setDrawable(getBaseContext().getResources().getDrawable(R.drawable.item_divider));
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        adapter = new AcceptedIncomingAdapter(bookData, this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onBookClick(int position, String owner) {
+        //open either location selector or previously selected location
+        Book book = bookData.get(position);
+        if (book.getLatitude().equals("")) {
+            //launch location selector
+            Intent intent = new Intent(this, MapActivity.class);
+            intent.putExtra("bookid", book.getBid());
+            intent.putExtra("borrower", book.getBorrower());
+            startActivity(intent);
+        } else {
+            //launch location viewer
+            Intent intent = new Intent(this, LocationActivity.class);
+            intent.putExtra("bookid", book.getBid());
+            startActivity(intent);
+        }
+    }
+
+
+
+}
