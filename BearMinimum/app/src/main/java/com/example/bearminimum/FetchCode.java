ebc61921a704a12cbf04320ddfc5634e0fde5926
Henry417E1610 <60022442+Henry417E1610@users.noreply.github.com>
2020-11-30 01:04:56 -0700

    made some clarification

--- BearMinimum/app/src/main/java/com/example/bearminimum/FetchCode.java
+++ BearMinimum/app/src/main/java/com/example/bearminimum/FetchCode.java
@@ -3,4 +3,5 @@ package com.example.bearminimum;
 import com.journeyapps.barcodescanner.CaptureActivity;
 
 public class FetchCode extends CaptureActivity {
+  // Used in scanBarcode for capturing activity as zxing package is used
 }
