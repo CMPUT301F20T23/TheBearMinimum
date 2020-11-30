e0ea0baebe8f6225d781340b2e08d629248f172f
rchin157 <rchin@ualberta.ca>
2020-11-30 12:19:01 -0700

    removed default tests, finished user tests

--- BearMinimum/app/src/test/java/com/example/bearminimum/UserTest.java
+++ BearMinimum/app/src/test/java/com/example/bearminimum/UserTest.java
@@ -1,10 +1,33 @@
 package com.example.bearminimum;
 
 import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
-    @Test
-    public void createUser() {
 
+    public User createMockUser() {
+        return new User("test@bearmin.com", "7801234567", "testUserid", "test");
+    }
+
+    @Test
+    public void checkUserAttributes() {
+        User user = createMockUser();
+        assertEquals(user.getEmail(), "test@bearmin.com");
+        assertEquals(user.getPhonenumber(), "7801234567");
+        assertEquals(user.getUid(), "testUserid");
+        assertEquals(user.getUsername(), "test");
+    }
+
+    @Test
+    public void checkSetAttributes() {
+        User user = createMockUser();
+        user.setEmail("test1@bearmin.com");
+        assertEquals(user.getEmail(), "test1@bearmin.com");
+        user.setPhonenumber("5871234567");
+        assertEquals(user.getPhonenumber(), "5871234567");
+        user.setUid("testuserid2");
+        assertEquals(user.getUid(), "testuserid2");
+        user.setUsername("tester");
+        assertEquals(user.getUsername(), "tester");
     }
 }
